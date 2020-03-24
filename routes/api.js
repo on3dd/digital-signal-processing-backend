b3f1a23b1ba04338d0b0fe4fc187a59869109f1f
on3dd <onedeadwave@gmail.com>
2020-03-24 21:18:36 +1000

    Started to create file processing

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+
+function processFile(buffer) {
+    // split the contents by new line
+    const lines = buffer.toString().split(/\r?\n/);
+
+    // print all lines
+    lines.forEach((line) => {
+        console.log(line);
+    });
+}
 
 /* GET users listing. */
 router.post('/file', function(req, res, next) {
-    console.log('fields:', req.fields);
-    console.log('files:', req.files);
+    fs.promises.readFile(req.files.file.path)
+        .then((buffer) => {
+            processFile(buffer);
+        })
+        .catch((err) => {
+           console.error(err);
+           res.status(500).send(err);
+        });
+
     res.send('respond with a resource');
 });
 
