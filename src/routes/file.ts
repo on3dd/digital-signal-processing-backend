a0723db34ba83b9ee0c48fb1f260aef433833df8
on3dd <onedeadwave@gmail.com>
2020-03-28 20:04:16 +1000

    Minor update

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,36 +1,40 @@
-import { Request, Response, Router } from 'express';
-import { BAD_REQUEST, OK } from 'http-status-codes';
-import { ParamsDictionary } from 'express-serve-static-core';
-import { paramMissingError } from '@shared/constants';
+import {Request, Response, Router} from 'express';
+import {BAD_REQUEST, OK, INTERNAL_SERVER_ERROR} from 'http-status-codes';
+import {ParamsDictionary} from 'express-serve-static-core';
+import {paramMissingError} from '@shared/constants';
 import * as fs from 'fs';
 
 // Init shared
 const router = Router();
 
 function processFile(buffer: Buffer) {
-  // split the contents by new line
-  const lines = buffer.toString().split(/\r?\n/);
+  return new Promise((resolve, reject) => {
+    const lines = buffer.toString().split(/\r?\n/);
 
-  // print all lines
-  lines.forEach((line) => {
-    console.log(line);
+    // print all lines
+    lines.forEach((line) => {
+      console.log(line);
+    });
+
+    resolve();
   });
 }
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   if (req.files === undefined) {
-    res.status(400).send('Request should contain files field \n');
+    res.status(BAD_REQUEST).send('Request should contain files field \n');
     return;
   }
 
   fs.promises.readFile(req.files.file.path)
-      .then((buffer) => {
-        processFile(buffer);
+      .then(async (buffer) => {
+        await processFile(buffer);
+        res.status(OK).send('OK \n');
       })
       .catch((err) => {
         console.error(err);
-        res.status(500).send(err);
+        res.status(INTERNAL_SERVER_ERROR).send(err);
       });
 });
 
