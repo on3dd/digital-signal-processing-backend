6e4eddac6e9b57652a30b14bf5567ac5a7f29aee
on3dd <onedeadwave@gmail.com>
2020-03-29 12:39:37 +1000

    Remove unused files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import UserRouter from './Users';
 import FileRouter from './file';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/users', UserRouter);
 router.use('/file', FileRouter);
 
 // Export the base-router
